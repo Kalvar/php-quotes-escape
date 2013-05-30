d4c14c46d417bd8110ffea4846c0de165c8d2c95
Kalvar <ilovekalvar@gmail.com>
2013-05-30 14:15:41 +0800

    Added a license header.

--- quotes.class.php
+++ quotes.class.php
@@ -1,5 +1,26 @@
 <?php
 /*
+  Copyright (c) 2013 Kuo-Ming Lin (http://kalvar-kaki.blogspot.tw/)
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+/*
  * @ Author  : Kuo-Ming Lin
  * @ Email   : ilovekalvar@gmail.com 
  * @ Version : V1.0
@@ -54,6 +75,13 @@ class Quotes
   public function unescapeHtmlString($_htmlString = '')
   {
     if( !$_htmlString ) return '';
+    /*
+     * @ Why double use str_replace() to unescapes &lt; and &gt;
+     *   - Because of the htmlspecialchars() will be escaping the < and > symbols actually, 
+     *     But it won't be really decode from using htmlspecialchars_decode(),
+     *     When it displays on browser that it'll be fine, 
+     *     But it'll be damaged when we need to catch the source code likes use json_encode() to response some web-service requests.
+     */
     $_unescaped = htmlspecialchars_decode($_htmlString, ENT_QUOTES);
     $_unescaped = str_replace('&lt;', '<', $_unescaped);
     $_unescaped = str_replace('&gt;', '>', $_unescaped);
